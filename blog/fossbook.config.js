d4604a0583eea0da16a6f06eec4df5777ece0748
HanaNovackova <hana_novackova@outlook.com>
2026-06-06 20:25:54 +0800

    first commit

---
+++ blog/fossbook.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  blogName: "My Blog",
+  authorName: "",
+  authorDescription: "",
+  authorWebsite: "",
+  blogDescription: "A blog powered by fossbook",
+  blogsite: "http://HanaNovackova.github.io/Hanina-zahrada/blog/",
+
+  // Optional
+  githubCNAME: "",
+  googleAnalyticsID: "",
+  authorTwitter: "",
+  siteTwitter: "",
+  githubRepository: "",
+  image: "",
+  theme: "archie",
+
+  // Comment system (optional)
+  // comments: { provider: "utterances", repo: "user/repo", issueTerm: "pathname", theme: "github-light" },
+
+  // Deployment
+  // deploy: { branch: "main", remote: "origin" },
+
+  // Directory overrides (defaults shown)
+  content: "./content",
+  postsDir: "./content/posts",
+  outputDir: "./public/blog",
+  staticDir: "./static",
+  themesDir: "./themes",
+};
